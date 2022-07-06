1560378c1ffd601df996478ce89da5390251fcad
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2022-07-06 15:41:39 -0400

    rewrite select_samples script

--- experiment-crawler/src/scripts/select_sample.py
+++ experiment-crawler/src/scripts/select_sample.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python3
-# The above tells bash or zah or whatever shell program you are using 
+# The above tells bash or zah or whatever shell program you are using
 # to use python3 to execute this.
 
+from typing import Iterable, Sequence
 import os
 import random
-import argparse 
+import argparse
+import itertools
 
-def read_filenames():
-    # use the args object to get the directory
-    # the command below should load in all of the filenames 
-    # in this directory, but the syntax might be off (e.g., listdir might be under os.path.listdir?)
-    files = os.listdir(args.directory)
-    return files
+def scan_dir(directory: str, recurse: bool) -> Iterable[str]:
+    for (dirpath, _, filenames) in itertools.islice(os.walk(directory, topdown=True),
+                                                    None if recurse else 1):
+        yield from (os.path.join(dirpath, f) for f in filenames)
 
-def select_files(filelist):
-    # randomly shuffle files
-    random.shuffle(filelist)
-    # return the first num_samples filenames
-    return filelist[:args.num_samples]
+def get_files(num_samples: int, directory: str) -> Iterable[str]:
+    files = scan_dir(directory, True)
+    # filtering?
+    return select_files(list(files), num_samples)
+
+def select_files(filelist: Sequence[str], num: int) -> Iterable[str]:
+    # Use unsafe PRNG.
+    return random.sample(filelist, num)
 
 def write_selected_files(filelist):
-    with open(file("samples.txt"), "w") as f:
+    with open("samples.txt", "w") as f:
         for warc in filelist:
             f.write(warc+"\n")
 
-if __name__ == "__main__":
-    # use argparse package to load in variables 
-    # right now, just load in number of samples
+def get_args():
     parser = argparse.ArgumentParser()
-    # look at the documentation to see how to specify that the 
-    # number of samples is a number
-    parser.add_argument("num_samples")
-    # directory is whereever or whatever lists each of the warc files
-    parser.add_argument("directory")
+    parser.add_argument("--num_samples", '-n',
+                        default=100,
+                        type=int,
+                        help="How many files to select.")
+    parser.add_argument("--directory", "-d",
+                        default=".",
+                        type=str,
+                        help="Directory to search for warc files.")
+    # Recursive? Read from file? Output to file? Verbosity?
+    return parser.parse_args()
 
-    args = parser.parse_args()
-    print("Namespace object", args)
-    print("Access elements of the namespace object", args.num_samples)
+def run() -> None:
+    args = get_args()
+    files = get_files(args.num_samples, args.directory)
+    for f in files:
+        print(os.path.abspath(f))
 
-    files = read_filenames()
-    print("read in {} files".format(len(files)))
-    selected = select_files(files)
-    assert len(selected) == args.num_samples
-    print("selected {} files".format(len(selected)))
-    write_selected_files(selected)
 
-    print("you should now have a list of {} files in samples.txt".format(len(selected)))
+if __name__ == "__main__":
+    run()
+
